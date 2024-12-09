f68b206c0e028f2e55fa429fb5e9f8105ad43ad4
Mark Elliott <115184056+Mark-Elliott5@users.noreply.github.com>
2024-12-09 06:05:33 -0500

    postRegister handler, validation, badrequestexception

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,5 +1,16 @@
 package com.example.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.entity.Account;
+import com.example.exception.BadRequestException;
+import com.example.service.AccountService;
+import com.example.service.MessageService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -7,6 +18,24 @@ package com.example.controller;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
+@RestController
 public class SocialMediaController {
 
+  @Autowired
+  private AccountService accountService;
+
+  @Autowired
+  private MessageService messageService;
+
+  @PostMapping("register")
+  public Account postRegister(@RequestBody Account account) {
+      //TODO: process POST request
+      int uLen = account.getUsername().length();
+      int pLen = account.getPassword().length();
+      if (uLen == 0) throw new BadRequestException("Username must not be blank.");
+      if (pLen < 4) throw new BadRequestException("Password must be at least 4 characters.");
+
+      return accountService.createAccount(account);
+  }
+  
 }
